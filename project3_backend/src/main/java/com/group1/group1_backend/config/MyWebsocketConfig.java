9d4eb4a228100f27b9e5b5dcff2adb7585771cf2
YangQing-Lin <linqingchuan0826@163.com>
2023-05-06 10:31:43 +0800

    暂存更改

---
+++ project3_backend/src/main/java/com/group1/group1_backend/config/MyWebsocketConfig.java
@@ -0,0 +1,26 @@
+package com.group1.group1_backend.config;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@Slf4j
+@EnableWebSocket
+public class MyWebsocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Autowired
+    WebSSHWebSocketHandler webSSHWebSocketHandler;
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        //socket通道
+        //指定处理器和路径，并设置跨域
+        webSocketHandlerRegistry.addHandler(webSSHWebSocketHandler, "/webssh")
+                .addInterceptors(new WebSocketInterceptor())
+                .setAllowedOrigins("*");
+    }
+}
